c0c40c0a142376f2f34bf191eae2a3fdc7d75bdb
gabriel.alves250597@gmail.com <you@example.com>
2023-04-28 16:53:27 -0300

    alterando

--- server.js
+++ server.js
@@ -1,21 +1,25 @@
 const jsonServer = require("json-server");
 const auth = require("json-server-auth");
 const cors = require("cors");
-const port = process.env.PORT || 3001; 
+const port = process.env.PORT || 3001;
 const routes = require('./routes.json');
-    
+
 const app = jsonServer.create();
 const router = jsonServer.router("db.json");
-    
+
 app.db = router.db;
-    
+
 const rules = auth.rewriter(
-routes);
-    
+    {
+        pets: 644,
+        users: 600,
+    }
+);
+
 app.use(cors());
 app.use(rules);
 app.use(auth);
 app.use(router);
 app.listen(port);
-    
+
 console.log("Server is running on port:", port);
